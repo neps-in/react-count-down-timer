f4d454e058c7b9b46272368ada325a2ef0e9c93c
Napoleon Arouldas <meetneps@gmail.com>
2022-06-15 21:43:14 +0530

    Working count down timer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import MyTimer from "./MyTimer";
 
 export default function App(){
-
     
-
     return(
         <>
-            <MyTimer startTime="0" startMini="2" startSec="30"/>
+            <MyTimer />
         </>
     )
 }
--- src/MyTimer.js
+++ src/MyTimer.js
@@ -1,43 +1,156 @@
-import React , {useEffect} from "react";
+import React , {useEffect, useState} from "react";
 
 export default function MyTimer(props){
 
+    const [formData, setFormData] = useState({
+        mini:0,
+        sec:0
+    })
+
     const [timeInfo, setTimeInfo] = React.useState({
-        mini:props.startMini,
-        sec:props.startSec,
-        totalTime:   Number(props.startMini * 60) + Number(props.startSec) 
+        mini:0,
+        sec:0,
+        totalTime:0
     })
     
-    function startTimer(){
-        const intervalId = setInterval( (prevtimeInfo) => {
+    const [pause, setPause] = useState(true);
 
-                // Update time
-                setTimeInfo(function(prevtimeInfo){
-                    if( timeInfo.totalTime > 0 ){
-                        console.log( 'totalTime : ' , timeInfo.totalTime );
-                        return {
-                            ...prevtimeInfo,
-                            totalTime : prevtimeInfo.totalTime - 1,
-                            mini: Math.floor(prevtimeInfo.totalTime / 60) ,
-                            sec:  prevtimeInfo.totalTime % 60
-                        }
-                    } else {
-                         clearInterval(intervalId)
-                    }
-                })
+    // function startTimer(){
+    //     const intervalId = setInterval( (prevtimeInfo) => {
+    //             console.log('About to start ...');
+    //             // Update time
+    //             setTimeInfo(function(prevtimeInfo){
+    //                 console.log( 'setTime info ',timeInfo.totalTime)
+    //                 if( timeInfo.totalTime > 0  ){
+    //                     console.log( 'totalTime : ' , timeInfo.totalTime );
+    //                     return {
+    //                         ...prevtimeInfo,
+    //                         totalTime : prevtimeInfo.totalTime - 1,
+    //                         mini: Math.floor(prevtimeInfo.totalTime / 60) ,
+    //                         sec:  Math.floor(prevtimeInfo.totalTime % 60)
+    //                     }
+    //                 } else if( pause ) {
+    //                     console.log( 'PAUSE CONDITION' )
+    //                     return {
+    //                         ...prevtimeInfo,
+    //                         totalTime : prevtimeInfo.totalTime ,
+    //                         mini: Math.floor(prevtimeInfo.totalTime / 60) ,
+    //                         sec:  prevtimeInfo.totalTime % 60
+    //                     }
+    //                 } else {
+    //                      clearInterval(intervalId)
+    //                 }
+    //             })
             
-        },1000)
+    //     },1000)
+    // }
+
+    useEffect(() => {
+        let interval = null;
+        if (!pause && timeInfo.totalTime !== -1 ) {
+            interval = setInterval(() => {
+
+            //Set Time info
+            setTimeInfo(function(prevtimeInfo){
+                    console.log( 'setTime info ',timeInfo.totalTime)
+                    console.log( 'totalTime : ' , timeInfo.totalTime );
+                    return {
+                        ...prevtimeInfo,
+                        totalTime : prevtimeInfo.totalTime - 1,
+                        mini: Math.floor(prevtimeInfo.totalTime / 60) ,
+                        sec:  Math.floor(prevtimeInfo.totalTime % 60)
+                    }
+                
+            })
+
+          }, 1000);
+        } else if (pause && timeInfo.totalTime !== 0) {
+          clearInterval(interval);
+        } 
+        return () => clearInterval(interval);
+      }, [pause, timeInfo.totalTime]);
+
+    
+
+    function handleToggle(event){
+        event.preventDefault()
+
+        setPause(prevPause => !prevPause);
+        console.log('toggle pause' , pause);
     }
 
-    useEffect(()=>{
-        startTimer()
-    })
-    
-    
+    function handleReset(event) {
+        event.preventDefault()
+
+        setTimeInfo(function(prevTimeInfo){
+
+            return {
+                ...prevTimeInfo,
+                totalTime:0,
+                mini:0,
+                sec:0
+            }
+        })
+        clearInterval()
+        setPause(false);
+    }
+
+    function handleChange(event){
+
+        const {name, value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value
+        }))
+
+    }
+
+
+    function handleStart(event){
+        event.preventDefault()
+        setPause(false);
+
+        if( formData.sec > 60 ){
+            formData.mini  = Number(formData.mini) +  Number(Math.floor( formData.sec / 60 ))
+            formData.sec   = Math.floor( formData.sec % 60 )
+            console.log( 'New formdata', formData )
+        }
+
+        setTimeInfo({
+            mini: formData.mini,
+            sec:formData.sec,
+            totalTime: Number(formData.mini * 60) + Number(formData.sec)
+        })
+        
+    }
+
     return(
         <>
             <h2>Time : {timeInfo.totalTime}</h2>
-            <h3>{timeInfo.mini}:{timeInfo.sec}</h3>
+            <h2>
+            {String(timeInfo.mini).padStart(2,'0')}:{String(timeInfo.sec).padStart(2,'0')}
+            </h2>
+            <h3>pause state: {pause ? "TRUE" : "FALSE" }</h3>
+            <form>
+                <input type="input"
+                       name="mini"
+                       onChange={handleChange} 
+                       value={formData.mini}/>
+                <input type="input"
+                       name="sec"
+                       onChange={handleChange} 
+                       value={formData.sec} />
+                <button type="submit"
+                        name="start"
+                        onClick={handleStart}>Start</button>
+                <button type="submit"
+                        name="pause"
+                        onClick={handleToggle}>Pause / Resume</button>
+                <button type="button"
+                        name="reset"
+                        onClick={handleReset}> Reset </button>
+                    
+            </form>
         </>
     )
 }
